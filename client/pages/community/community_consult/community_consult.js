adb99d97833dcd88ec0739ef522515b3763e3c3c
1159717508@qq.com <1159717508@qq.com>
2018-08-05 09:58:57 +0800

    法律社区-->咨询律师

---
+++ client/pages/community/community_consult/community_consult.js
@@ -0,0 +1,102 @@
+// pages/community/community_consult/community_consult.js
+Page({
+   
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      img: '../../images/banner.jpg',
+      more:'>',
+      classifyList:[
+          {
+            case: '婚姻家庭纠纷',
+            caseDetail:'婚姻家庭纠纷、离婚纠纷、夫妻财产约定纠纷、抚养纠纷、赡 养纠纷、监护权纠纷等                           其他婚姻家庭纠纷',
+            num:26
+          },
+          {
+              case: '继承纠纷',
+              caseDetail: '法定继承纠纷、遗嘱继承纠纷、遗赠纠纷、遗赠抚养协议纠纷 等其他继承相关纠纷',
+              num:10
+          },
+          {
+              case: '所有权纠纷',
+              caseDetail: '相邻关系纠纷、建筑物区分所有权纠纷、业主知情权纠纷、共 有纠纷等其他所有权相                         关纠纷',
+              num:8
+          },
+          {
+              case: '知识产权权属、侵权纠纷',
+              caseDetail: '著作权权属、侵权纠纷；专利权权属、侵权纠纷；发明权纠纷； 发明权纠纷；发现权                         纠纷等其他知识产权相关纠纷',
+              num:6
+          },
+          {
+              case: '劳动争议、人事争议',
+              caseDetail: '劳动合同纠纷、社会保险纠纷、福利待遇纠纷、人事争议等其 他劳动和人事相关纠纷',
+              num:6
+          },
+          {
+              case: '侵权责任纠纷',
+              caseDetail: '监护人责任纠纷、教育机构责任纠纷、网格侵权责任纠纷、用 人单位责任纠纷等其他                         侵权责任纠纷',
+              num:12
+          }
+      ],
+      lawyersList:[
+        
+      ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
