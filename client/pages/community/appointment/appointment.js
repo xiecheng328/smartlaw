492b655cd8662a015d2a24467e1fc1a694da2bcd
ZHENGXU11 <zx123456>
2018-08-07 21:46:38 +0800

    zx

--- client/pages/community/appointment/appointment.js
+++ client/pages/community/appointment/appointment.js
@@ -12,8 +12,27 @@ Page({
     timeT: '12:00',
     indexT: 0,
     region: ['广东省', '广州市', '海珠区'],
-    customItem: '全部'
-  
+    customItem: '全部',
+    array: ['婚姻类', '经济类', '刑事类', '维权类'],
+    objectArray: [
+      {
+        id: 0,
+        name: '婚姻类'
+      },
+      {
+        id: 1,
+        name: '经济类'
+      },
+      {
+        id: 2,
+        name: '刑事类'
+      },
+      {
+        id: 3,
+        name: '维权类'
+      }
+    ],
+    indexLocal: 0,
   },
   bindTimeChange: function (e) {
     console.log("谁哦按")
@@ -46,7 +65,12 @@ Page({
       region: e.detail.value
     })
   },
-
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      indexLocal: e.detail.value
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
