e03efec1bf5f4cf9f42cf99d5ffc82e0e1c0a638
Yutao's git <969585912@qq.com>
2018-08-09 15:43:00 +0800

    question完成

--- client/pages/community/question/questionDetails/questionDetails.js
+++ client/pages/community/question/questionDetails/questionDetails.js
@@ -5,62 +5,103 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    array: [
+      {
+        header: "一号律师   一号律师事务所",
+        accept: "已采纳",
+        src: "../../../images/header-icon.png",
+        iconImg: "../../../images/麦克风.png",
+        listenIn: "点击收听",
+        number: "37人收听"
+      },
+      {
+        header: "一号律师   一号律师事务所",
+        accept: "已采纳",
+        src: "../../../images/header-icon.png",
+        iconImg: "../../../images/麦克风.png",
+        listenIn: "点击收听",
+        number: "37人收听"
+      },
+      {
+        header: "一号律师   一号律师事务所",
+        accept: "已采纳",
+        src: "../../../images/header-icon.png",
+        iconImg: "../../../images/麦克风.png",
+        listenIn: "点击收听",
+        number: "37人收听"
+      },
+      {
+        header: "一号律师   一号律师事务所",
+        accept: "已采纳",
+        src: "../../../images/header-icon.png",
+        iconImg: "../../../images/麦克风.png",
+        listenIn: "点击收听",
+        number: "37人收听"
+      },
+      {
+        header: "一号律师   一号律师事务所",
+        accept: "已采纳",
+        src: "../../../images/header-icon.png",
+        iconImg: "../../../images/麦克风.png",
+        listenIn: "点击收听",
+        number: "37人收听"
+      },
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
