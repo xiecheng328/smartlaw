c14fbae858fd851199b746386a5c4c34efa04048
ZHENGXU11 <zx123456>
2018-08-11 21:29:29 +0800

    跳转完成
    Signed-off-by: ZHENGXU11 <zx123456>

---
+++ client/pages/community/appointment_content/appointment_content.js
@@ -0,0 +1,105 @@
+// pages/community/appointment_content/appointment_content.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    region: ['广东省', '广州市', '海珠区'],
+    customItem: '全部',
+    array: ['婚姻类', '经济类', '刑事类', '维权类'],
+    objectArray: [
+      {
+        id: 0,
+        name: '婚姻类'
+      },
+      {
+        id: 1,
+        name: '经济类'
+      },
+      {
+        id: 2,
+        name: '刑事类'
+      },
+      {
+        id: 3,
+        name: '维权类'
+      }
+    ],
+    indexLocal: 0,
+  },
+  bindDateChangeT: function (e) {
+    console.log(e.detail.value)
+    this.setData({
+      dateT: e.detail.value
+    })
+  },
+  bindRegionChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      region: e.detail.value
+    })
+  },
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      indexLocal: e.detail.value
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
